ad237c5681f54727883efa68fb75bd0c0b22164b
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2023-11-12 08:25:35 +0530

    Update ContentPage.js

--- src/Component/ContentPage.js
+++ src/Component/ContentPage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { server } from "../index";
+import "./ContentPageStyles.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const ContentPage = () => {
   const { contentId } = useParams();
@@ -20,15 +23,24 @@ const ContentPage = () => {
   }, [contentId]);
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <img src={post.imgUrl} alt={post.title} />
 
+   
+    <div className="content-body">
+    <Navbar post={Navbar} />
+    <section className="uppersection">
+     <a href="index.html" class="back-home">Back To Home</a>
+      <h1>{post.title}</h1>
+      
+      <img className="postpic" src={post.imgUrl} alt={post.title} />
+      </section>
       {/* Render Quill content using dangerouslySetInnerHTML */}
+      <section className="bottomsection">
       <div dangerouslySetInnerHTML={{ __html: post.content }} />
 
       {/* Other content fields can be rendered as normal */}
       <p>{post.otherField}</p>
+      </section>
+      <Footer post={Footer} />
     </div>
   );
 };

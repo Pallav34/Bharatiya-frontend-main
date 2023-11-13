018d9031991df5eb876e14b2b0a5255241f35fe3
Pallav34 <pallavbag1@gmail.com>
2023-11-13 16:19:53 +0530

    bug fixed

--- src/Component/PostCard.js
+++ src/Component/PostCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PostCard = ({ post }) => {
-  const { _id, title, content, imgUrl } = post; // Assuming you have an 'imgUrl' property in your post data.
+  const { _id, title, content, imgUrl } = post; 
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
@@ -11,8 +11,8 @@ const PostCard = ({ post }) => {
         <div className="font-bold text-xl mb-2">{title}</div>
         
         {/* Render Quill content using dangerouslySetInnerHTML */}
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-        
+        <div dangerouslySetInnerHTML={{ __html: content.split(' ').slice(0, 50).join(' ') }} />
+        ...
       </div>
       <div className="px-6 pt-4 pb-2">
         <Link to={`/content/${_id}`} className="text-indigo-600 hover:text-indigo-900">

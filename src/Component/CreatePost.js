66334b81d2c24d4b1f1fb6f949ed370cb902f55e
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2023-11-15 20:18:56 +0530

    Update CreatePost.js

--- src/Component/CreatePost.js
+++ src/Component/CreatePost.js
@@ -2,30 +2,32 @@ import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Quill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import axios from 'axios'; 
+import axios from 'axios';
 import { Context, server } from "../index";
 import { Navigate } from 'react-router-dom';
-const CreatePost = () => {
-    const { isAuthenticated } = useContext(Context);
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [content, setContent] = useState(''); // State to manage Quill content
 
-  
+const CreatePost = () => {
+  const { isAuthenticated } = useContext(Context);
+  const { register, handleSubmit, formState: { errors }, reset } = useForm(); // Get the reset function
+
+  const [content, setContent] = useState('');
+
   const onSubmit = async (data, e) => {
-    e.preventDefault(); // Prevent page reload
-  
+    e.preventDefault();
+
     try {
-      const postData = { ...data, content }; // Include content from Quill editor in the data
-  
+      const postData = { ...data, content };
+
       const response = await axios.post(`${server}/blogs/new`, postData, {
         headers: {
           'Content-Type': 'application/json',
         },
         withCredentials: true,
       });
-  
+
       if (response.status === 201) {
         console.log('Blog post created successfully.');
+        reset(); // Reset the form after successful submission
       } else {
         console.error('Error creating blog post.');
       }
@@ -34,12 +36,12 @@ const CreatePost = () => {
     }
   };
 
-
   const handleContentChange = (value) => {
     setContent(value);
   };
 
   if (!isAuthenticated) return <h1>Login first</h1>;
+
   return (
     <div className="max-w-md mx-auto">
       <h2 className="text-2xl font-semibold mb-4">Create a New Blog Post</h2>
